55acd75af861b2ddf9178459265ca4e5490719bd
Joksan Telles <ioksan.telles@gmail.com>
2020-10-26 12:17:46 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package JoksanTelles
+ * @subpackage Joksan_Theme
+ * @since Joksan Theme 1.0
+ */
+
+// Esto hace que aparezca el header del panel de administración
+get_header();
+
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <!-- Configuración general SEO -->
+    <meta charset="UTF-8">
+    <title>Website Theme</title>
+    <meta name="author" content="RIN x RAM">
+    <meta name="description" content="Meta Description">
+
+    <!-- Compatibilidad y responsive design -->
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="HandheldFriendly" content="true">
+    <link rel="shortcut icon" type="image/png" href="./img/favicon.png">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+    <!-- CSS Extra Style -->
+    <link rel="stylesheet" href="./assets/css/main.css">
+</head>
+<body>
+	
+</body>
+</hmtl>
